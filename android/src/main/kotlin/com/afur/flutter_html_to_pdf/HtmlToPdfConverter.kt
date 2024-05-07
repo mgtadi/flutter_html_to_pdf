f0bcc734fff25fcaa15060425d3b71a1acde08a9
mgtadi <mgtadi@users.noreply.github.com>
2024-05-07 20:27:16 +0630

    Update HtmlToPdfConverter.kt

--- android/src/main/kotlin/com/afur/flutter_html_to_pdf/HtmlToPdfConverter.kt
+++ android/src/main/kotlin/com/afur/flutter_html_to_pdf/HtmlToPdfConverter.kt
@@ -77,7 +77,7 @@ class HtmlToPdfConverter {
             if (printSize == "mm58") {
                
                  mediaSize = PrintAttributes.MediaSize("flutter_printing", "Provided size",
-                            ( 225 * 1000.0 / 72.0).toInt(),
+                            ( 208 * 1000.0 / 72.0).toInt(),
                            ( 310 * 1000.0 / 72.0).toInt());
             }
             val attributes = PrintAttributes.Builder()
