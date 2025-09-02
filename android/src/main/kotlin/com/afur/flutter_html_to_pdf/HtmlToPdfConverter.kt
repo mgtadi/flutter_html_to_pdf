bb751ab4daefe023bfc5e81e5a8a17f69c9454d6
mgtadi <mgtadi@users.noreply.github.com>
2025-09-02 13:50:27 +0630

    Update HtmlToPdfConverter.kt

--- android/src/main/kotlin/com/afur/flutter_html_to_pdf/HtmlToPdfConverter.kt
+++ android/src/main/kotlin/com/afur/flutter_html_to_pdf/HtmlToPdfConverter.kt
@@ -34,6 +34,66 @@ class HtmlToPdfConverter {
         }
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
+    fun convertIncWidthAndHeight(filePath: String, applicationContext: Context, printSize: String, orientation: String, width: int,
+                                 height: int, callback: Callback) {
+        val webView = WebView(applicationContext)
+        val htmlContent = File(filePath).readText(Charsets.UTF_8)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+        webView.settings.allowFileAccess = true
+        webView.loadDataWithBaseURL(null, htmlContent, "text/HTML", "UTF-8", null)
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                super.onPageFinished(view, url)
+                createPdfFromWebViewIncWidthandHeight(webView, applicationContext, printSize, width, height, orientation, callback)
+            }
+        }
+    }
+
+    fun createPdfFromWebViewIncWidthandHeight(webView: WebView, applicationContext: Context, printSize: String, orientation: String, width: int,
+                                              height: int, callback: Callback) {
+        val path = applicationContext.filesDir
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            var mediaSize = PrintAttributes.MediaSize.ISO_A4
+
+            when (orientation) {
+                "LANDSCAPE" -> mediaSize = mediaSize.asLandscape()
+                "PORTRAIT" -> mediaSize = mediaSize.asPortrait()
+            }
+            var margins = PrintAttributes.Margins(
+                            (5 * 1000.0).toInt(),
+                            (5 * 1000.0 / 72.0).toInt(),
+                            (5 * 1000.0 / 72.0).toInt(),
+                            (5 * 1000.0 / 72.0).toInt());
+            
+            mediaSize = PrintAttributes.MediaSize("flutter_printing", "Provided size",
+                            ( width * 1000.0 / 72.0).toInt(),
+                           ( height * 1000.0 / 72.0).toInt());
+            val attributes = PrintAttributes.Builder()
+                .setMediaSize(mediaSize)
+                .setResolution(PrintAttributes.Resolution("pdf", "pdf", 100, 100))
+                .setMinMargins(PrintAttributes.Margins.NO_MARGINS).build();
+            webView.getSettings().setTextZoom(100);
+
+            val printer = PdfPrinter(attributes);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                val adapter = webView.createPrintDocumentAdapter(temporaryDocumentName)
+
+                printer.print(adapter, path, temporaryFileName, object : PdfPrinter.Callback {
+                    override fun onSuccess(filePath: String) {
+                        callback.onSuccess(filePath)
+                    }
+
+                    override fun onFailure() {
+                        callback.onFailure()
+                    }
+                })
+            }
+        }
+    }
+
     fun createPdfFromWebView(webView: WebView, applicationContext: Context, printSize: String, orientation: String, callback: Callback) {
         val path = applicationContext.filesDir
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
